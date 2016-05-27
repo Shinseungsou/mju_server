66c2fd9dd52abab141154becec1c953c59ce4b5c
Shinseungsou <wwww432@lycos.co.kr>
2016-05-27 19:54:09 +0900

    add signup function

--- controller/users.py
+++ controller/users.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, current_app
+from flask import Blueprint, request
+import json
 
 # from config.mjudb import mjudb
 from controller import db
@@ -9,16 +10,47 @@ users = Blueprint('users', __name__, url_prefix="/users")
 def users_index():
     return "{\"user\":{\"name\":\"Shin\",\"age\":\"24\"}}"
 
-@users.route("/signup")
-def signup():
+@users.route("/signin")
+def signin():
     # mjudbs = mjudb().setDB(current_app).getDB()
     cursor = db.connect().cursor()
     cursor.execute("select * from users")
     result = []
+    columns = tuple([d[0] for d in cursor.description])
 
     for row in cursor:
-        result.append(row)
+        result.append(dict(zip(columns, row)))
 
     print result
 
-    return "hello"
+    return json.dumps(result)
+
+@users.route("/signup", methods=["POST"])
+def signup():
+    # if(request.method == 'POST'):
+    username = request.args.get('username')
+    pw = request.args.get('pw')
+    id = request.args.get('id')
+    pn = request.args.get('pn')
+    nickname = request.args.get('nick')
+    email = request.args.get('email')
+    gender = request.args.get('gender')
+    print "hello"
+    # pw = request.form['pw']
+    # id = request.form['id']
+    # pn = request.form['pn']
+    # nickname = request.form['nick']
+    # email = request.form['email']
+
+    connection = db.connect()
+    cursor = connection.cursor()
+    query = "insert into " \
+            "users(id,pw,username,nickname,phonenumber,email) " \
+            "values('"+id+"','"+pw+"','"+username+"','"+nickname+"','"+pn+"','"+email+"');"
+
+    cursor.execute(query)
+    connection.commit()
+
+    return '"result":"success"'
+    # else:
+    #     return '"result":"fail"'
