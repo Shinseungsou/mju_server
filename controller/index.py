bc584e2417f13641479a689df8e5ad7694e7a36d
SSS <wwww43211@gmail.com>
2016-11-16 02:35:40 +0900

    Add jinja

--- controller/index.py
+++ controller/index.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template
+from controller import db
 
 index = Blueprint('index', __name__, url_prefix="/")
 
 @index.route("/")
 def index_index():
-    return render_template("index.html")
+    conn = db.connect()
+    cursor = conn.cursor()
+    cursor.execute("select * from user")
+    users = []
+    for user in cursor:
+        users.append(user[1])
+    print users
+    return render_template("index.xhtml", title='hello')
